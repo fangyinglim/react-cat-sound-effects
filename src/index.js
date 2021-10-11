c0d569f8f2e8e26f95fce2530a9f84bb951fe3ad
fangyinglim <limfy97@gmail.com>
2021-10-11 18:05:04 +0800

    adding files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDom from 'react-dom';
+import './index.css'
+import { notes } from './notes';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Violin() {
+  return (
+    <div className='flexbox'>
+    {notes.map( (eachNote) => {
+      // console.log(eachNote);
+      // const {key, note, audio} = eachNote
+      return <MusicNotes key={eachNote.key} eachNote={eachNote} /> 
+    })}
+    </div>
+    );
+  }
+  
+  
+  const MusicNotes = (props) => {
+    const {keycode, note, audio} = props.eachNote;
+    // console.log(props.eachNote);
+    
+    const playAudio = (e) => {
+      // console.log(audio);
+      // console.log(inputRef.current.getAttribute('kbkey'));
+      // console.log(e.key);
+
+      if (inputRef.current.getAttribute('kbkey') === e.key){
+        let audioToPlay = new Audio(audio);
+        audioToPlay.currentTime = 2;
+        audioToPlay.play();
+      }
+    }
+
+    const inputRef = useRef();
+
+    useEffect( ()=> {
+      document.addEventListener('keypress', playAudio)
+      return () => window.removeEventListener('keypress', playAudio)
+    }, )
+
+    return (
+      <div ref={inputRef} kbkey={keycode} className='musicNote' onClick={playAudio}> Note {note.toUpperCase()} 
+        <p> Press {note}  to play</p>
+  </div>
+  )
+} 
+
+
+ReactDom.render(<Violin />, document.getElementById('root'));
---
+++ src/notes.js
@@ -0,0 +1,50 @@
+export const notes = [
+    {
+        key: 'c',
+        keycode: 'c',
+        note: 'c',
+        audio: 'https://www.ee.columbia.edu/~dpwe/sounds/instruments/violin-C4.wav',
+    },
+
+    {
+        key: 'd',
+        keycode: 'd',
+        note: 'd',
+        audio: 'https://www.ee.columbia.edu/~dpwe/sounds/instruments/violin-C4.wav',
+    },
+
+    {
+        key: 'e',
+        keycode: 'e',
+        note: 'e',
+        audio: 'https://www.ee.columbia.edu/~dpwe/sounds/instruments/violin-C4.wav',
+    },
+
+    {
+        key: 'f',
+        keycode: 'f',
+        note: 'f',
+        audio: 'https://www.ee.columbia.edu/~dpwe/sounds/instruments/violin-C4.wav',
+    },
+
+    {
+        key: 'g',
+        keycode: 'g',
+        note: 'g',
+        audio: 'https://www.ee.columbia.edu/~dpwe/sounds/instruments/violin-C4.wav',
+    },
+
+    {
+        key: 'a',
+        keycode: 'a',
+        note: 'a',
+        audio: 'https://www.ee.columbia.edu/~dpwe/sounds/instruments/violin-C4.wav',
+    },
+
+    {
+        key: 'b',
+        keycode: 'b',
+        note: 'b',
+        audio: 'https://www.ee.columbia.edu/~dpwe/sounds/instruments/violin-C4.wav',
+    }
+]
